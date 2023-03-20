e83857594adb917d28e9476c1b9068d921007542
sparkmahadi <mahadi.development@gmail.com>
2023-03-20 10:24:45 +0600

    added more apis

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-    const articlesCollection = client.db("dailyPlanet").collection("articles");
+    const articlesCollection = client.db("dailyMoon").collection("articles");
 
     app.get('/articles', async(req, res)=>{
         const query = {};
@@ -28,11 +28,46 @@ async function run(){
 
     app.get('/articles/:id', async(req, res)=>{
         const id = req.params.id;
-        console.log(id);
+        console.log('article for view', id);
         const query = { _id: new ObjectId(id)};
         const article = await articlesCollection.findOne(query);
         res.send(article)
     });
+
+    app.post("/articles/add-article", async(req, res)=>{
+        const article = req.body;
+        console.log('body for insert', article);
+        const result = await articlesCollection.insertOne(article);
+        res.send(result);
+    });
+
+    app.delete("/articles/:id", async(req, res)=>{
+        const id = req.params.id;
+        console.log('id for delete', id);
+        const query = {_id : new ObjectId(id)}
+        const result = await articlesCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    app.put("/articles/:id", async(req, res)=>{
+        const id= req.params.id;
+        const data = req.body;
+        const query = { _id: new ObjectId(id)};
+        const options = {upsert : true};
+        const newArticle = {
+            $set:{
+                author: data.author,
+                content: data.content,
+                title: data.title,
+                urlToImage: data.urlToImage,
+                tags: data.tags,
+            }
+        };
+
+        const result = await articlesCollection.updateOne(query, newArticle, options);
+        res.send(result);
+    })
+
 }
 
 run().catch(error => console.log(error));
